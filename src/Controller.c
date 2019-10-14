46e1f57149c307909358cd49a4bfc221399c3a07
Joey Kilgore <joey.a.kilgore@gmail.com>
2019-10-14 15:27:21 -0400

    Update Controller.c
    cleaned up comments and documentation

--- src/Controller.c
+++ src/Controller.c
@@ -24,7 +24,7 @@ int main()
 
     int highest = 0;
 
-    //printf("Hey1\n");
+    // run through 100 generation of networks
     for(int genNum=0; genNum<100; genNum++){
         printf("GEN NUM: %d\n",genNum);
         double scores[genSize];
@@ -34,19 +34,12 @@ int main()
             for (int i = 0; i < genSize; i++)
             {
                 printf("SPECIES: %d\n", i);
-                //printf("Hey 123\ns");
                 scores[i] = testNetwork(&gen[i], data);
             }
         }
         
-
-        //printf("Hey2\n");
-
-        
         for (int i = 1; i < genSize; i++)
         {
-            //printf("checking %d against %d\n", i, highest);
-            //printf("scores %f and %f\n", scores[i], scores[highest]);
             if (scores[i] > scores[highest])
             {
                 highest = i;
@@ -62,7 +55,6 @@ int main()
         double bestScore = testNetwork(&(gen[highest]), data);
         printf("READY TO WRITE TO FILE\n");
         printf("# APs, %f\n", bestScore);
-        // MAKE SURE THE FILE IS CLOSED!
         char buf[20];
         snprintf(buf, 20, "gen%02d.csv", genNum);
         writeToFile(data, buf);
@@ -70,16 +62,18 @@ int main()
 }
 
 double testNetwork(NeuralNetwork *NN, float data[10000][205])
-{
+{   // testNetwork takes in a neural network that is meant to generate a set of extracellular voltages
+    //  and gives it a score based on how well it blocks APs
+    // This is done by having a continuous injected current (to cause APs) and then
+    //  checking for spikes on the opposite end of the axon. Every AP measured means the NN loses a point
+
+    // Setup the environment
     double score = 0;
     struct simEnv env;
-    //env.compartments = malloc(51*sizeof(struct compartment));
     for (int i = 0; i < 51; i++)
     {
         // set initial conditions for each compartment
         // init v=-65,m=.052,h=.596,n=.317
-        // env.compartments[i];
-        //printf("%d\n",i);
         env.compartments[i] = (struct compartment *)malloc(sizeof(struct compartment));
 
         env.compartments[i]->v = -65.0;
@@ -89,22 +83,14 @@ double testNetwork(NeuralNetwork *NN, float data[10000][205])
         env.compartments[i]->n = .317;
 
         env.compartments[i]->vext = 0;
-        //printf("sad\n");
     }
-
-    
-    //sprintf("Hey 5\n");
-
     for (int i = 0; i < 51; i++)
     {
         env.compartments[i]->left = (i > 0) ? env.compartments[i - 1] : NULL;
         env.compartments[i]->right = (i < 50) ? env.compartments[i + 1] : NULL;
     }
 
-    //printf("Hey 67\n");
-
-    // TEST 1
-    // MINIMUM FIRING FROM WAVEFORM
+    // Define the injected current
     float inject[51];
     for (int i = 0; i < 51; i++)
     {
@@ -112,19 +98,23 @@ double testNetwork(NeuralNetwork *NN, float data[10000][205])
     }
     inject[0] = 10.0;
 
-    //printf("Hey 59\n");
+    // Run the trial
     bool firing = false;
     for (int timeSteps = 0; timeSteps < 10000; timeSteps++)
     {
-        //printf("Hey 60\n");
+        // calculate the extracellular voltages
         calcFwd(NN);
+        
+        // Set the extracellular voltage for center 11 compartments
         for (int i = 0; i <= 10; i++)
         {
             env.compartments[i + 20]->vext = NN->vExt[i];
         }
-        //printf("Hey 61\n");
+
+        // take a single time step (.01ms)
         takeTimeStep(env, .01, inject, timeSteps, data);
-        //printf("Hey 104\n");
+
+        // check for firing in the final compartment
         if (!firing && env.compartments[50]->v > 0)
         {
             firing = true;
@@ -134,7 +124,6 @@ double testNetwork(NeuralNetwork *NN, float data[10000][205])
         {
             firing = false;
         }
-        //printf("Hey 62\n");
     }
     return score;
 }
